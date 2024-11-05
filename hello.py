fa28065ad0f96c2f8c42b419701b5ae1d5866e6e
belkisarslan <belkisarslan33@gmail.com>
2024-11-05 17:49:53 +0300

    hello python

---
+++ hello.py
@@ -0,0 +1,34 @@
+# Bir sayının karesini alma
+# def squ(a):
+#     return a**2
+# b = int(input("Lütfen bir sayı giriniz: "))
+# print(f"Girdiğiniz {b} sayısının karesi {squ(b)}")
+
+
+# iki sayının çarpımını göster
+# def multi(a,b):
+#     return a*b
+# a = int(input("Lütfen ilk sayıyı giriniz: "))
+# b = int(input("Lütfen ikinci sayıyı giriniz: "))
+# print(f"Girdiğiniz iki sayının çarpımı {multi(a,b)}")
+
+
+# liste elemanlarının çarpımı
+# list= [1,2,3,4,5]
+# x = 1
+# for item in list:
+#     x *= item
+# print(int(x)) 
+
+
+# faktöriyel alma
+# def fak(a):
+#     if a == 0:
+#         return 1
+#     else:
+#         return a * fak(a-1) 
+# a = int(input("Lütfen bir sayı giriniz: "))
+# print(f"Girdiğiniz sayının faktöriyeli {fak(a)}")
+
+
+   
