d16a4d1b343c0077cac8f3c4c2823dd63e209818
belkisarslan <belkisarslan33@gmail.com>
2024-11-07 14:40:25 +0300

    module

---
+++ module.py
@@ -0,0 +1,6 @@
+string_text = "Bu string module dosyasından gelmektedir."
+def sum(number):
+    total = 0
+    for x in number:
+        total += x
+    return total    
---
+++ program.py
@@ -0,0 +1,8 @@
+import module
+from module import string_text
+
+
+number = [1,2,3,4,5]
+sumtotal = module.sum(number)
+print(sumtotal)
+print(string_text)
